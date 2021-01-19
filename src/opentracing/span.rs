0dd0095b935527c56237c8cf698f7bc81590851a
Aleksandr Ovchinnikov <mail@mr1sunshine.com>
2021-01-19 06:22:39 +0200

    Some more work

--- src/opentracing/span.rs
+++ src/opentracing/span.rs
@@ -1,3 +1,5 @@
+use eyre::Result;
+
 /// SpanContext represents Span state that must propagate to descendant Spans and
 /// across process boundaries (e.g., a <trace_id, span_id, sampled> tuple).
 pub(crate) trait SpanContext<F>
@@ -7,5 +9,5 @@ where
     /// ForeachBaggageItem calls a function for each baggage item in the
     /// context.  If the function returns false, it will not be called
     /// again and ForeachBaggageItem will return.
-    fn foreach_baggage_item(f: F);
+    fn foreach_baggage_item(&self, f: F) -> Result<()>;
 }
--- src/propagation.rs
+++ src/propagation.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, sync::Mutex};
-
 use crate::{opentracing, sampling_priority::SamplingPriority};
+use eyre::{eyre, Result};
+use std::{collections::HashMap, sync::Mutex};
 
 pub(crate) struct SpanContext {
     nginx_opentracing_compatibility_hack: bool,
@@ -39,13 +39,76 @@ impl SpanContext {
             ..SpanContext::new(id, trace_id, "", baggage)
         }
     }
+
+    pub fn id(&self) -> u64 {
+        self.id
+    }
+
+    pub fn trace_id(&self) -> u64 {
+        self.trace_id
+    }
+
+    pub fn propagated_sampling_priority(&self) -> &Option<SamplingPriority> {
+        &self.propagated_sampling_priority
+    }
+
+    pub fn origin(&self) -> &str {
+        &self.origin
+    }
+
+    pub fn set_baggage_item(&mut self, key: &str, value: &str) -> Result<()> {
+        let mut data = self
+            .baggage
+            .lock()
+            .map_err(|_| eyre!("mutex lock failed"))?;
+
+        data.insert(String::from(key), String::from(value));
+
+        Ok(())
+    }
+
+    pub fn baggage_item(&self, key: &str) -> Result<Option<String>> {
+        let data = self
+            .baggage
+            .lock()
+            .map_err(|_| eyre!("mutex lock failed"))?;
+
+        Ok(match data.get(key) {
+            Some(str) => Some(str.clone()),
+            None => None,
+        })
+    }
+
+    pub fn with_id(&self, id: u64) -> Result<SpanContext> {
+        let data = self
+            .baggage
+            .lock()
+            .map_err(|_| eyre!("mutex lock failed"))?;
+
+        let baggage = data.clone();
+        let mut context = SpanContext::new(id, self.trace_id, &self.origin, baggage);
+        context.propagated_sampling_priority = self.propagated_sampling_priority.clone();
+
+        Ok(context)
+    }
 }
 
 impl<F> opentracing::SpanContext<F> for SpanContext
 where
     F: Fn(&str, &str) -> bool,
 {
-    fn foreach_baggage_item(f: F) {
-        todo!()
+    fn foreach_baggage_item(&self, f: F) -> Result<()> {
+        let data = self
+            .baggage
+            .lock()
+            .map_err(|_| eyre!("mutex lock failed"))?;
+
+        for (key, value) in data.iter() {
+            if !f(key, value) {
+                return Ok(());
+            }
+        }
+
+        Ok(())
     }
 }
