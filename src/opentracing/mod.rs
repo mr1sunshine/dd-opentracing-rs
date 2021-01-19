ed7e856beea0d85feade50597616ff2ca441dfc4
Aleksandr Ovchinnikov <mail@mr1sunshine.com>
2021-01-19 07:40:28 +0200

    Add opentracing Propagation types

--- src/opentracing/mod.rs
+++ src/opentracing/mod.rs
@@ -1,3 +1,7 @@
+mod propagation;
 mod span;
+mod tracer;
 
+pub(crate) use propagation::*;
 pub(crate) use span::*;
+pub(crate) use tracer::*;
---
+++ src/opentracing/propagation.rs
@@ -0,0 +1,108 @@
+use eyre::Result;
+
+use super::{SpanContext, Tracer};
+
+pub(crate) enum PropagationError {
+    /// `InvalidSpanContext` occurs when Tracer::Inject() is asked to operate
+    /// on a SpanContext which it is not prepared to handle (for example, since it
+    /// was created by a different tracer implementation).
+    InvalidSpanContext,
+    /// `InvalidCarrier` occurs when Tracer::Inject() or Tracer::Extract()
+    /// implementations expect a different type of `carrier` than they are given.
+    InvalidCarrier,
+    /// `SpanContextCorrupted` occurs when the `carrier` passed to
+    /// Tracer::Extract() is of the expected type but is corrupted.
+    SpanContextCorrupted,
+    /// `KeyNotFound` occurs when TextMapReader::LookupKey fails to find
+    /// an entry for the provided key.
+    KeyNotFound,
+    /// `LookupKeyNotSupported` occurs when TextMapReader::LookupKey is
+    /// not supported for the provided key.
+    LookupKeyNotSupported,
+}
+
+/// TextMapReader is the Extract() carrier for the TextMap builtin format. With
+/// it, the caller can decode a SpanContext from entries in a propagated map of
+/// Unicode strings.
+///
+/// See the HTTPHeaders examples.
+pub(crate) trait TextMapReader<F>
+where
+    F: Fn(&str, &str) -> Result<()>,
+{
+    /// LookupKey returns the value for the specified `key` if available. If no
+    /// such key is present, it returns `PropagationError::KeyNotFound`.
+    ///
+    /// TextMapReaders are not required to implement this method. If not supported,
+    /// the function returns `PropagationError::LookupKeyNotSupported`.
+    ///
+    /// Tracers may use this as an alternative to `ForeachKey` as a faster way to
+    /// extract span context.
+    fn lookup_key(&self, key: &str) -> Result<String, PropagationError>;
+
+    /// ForeachKey returns TextMap contents via repeated calls to the `f`
+    /// function. If any call to `f` returns an error, ForeachKey terminates and
+    /// returns that error.
+    ///
+    /// NOTE: The backing store for the TextMapReader may contain data unrelated
+    /// to SpanContext. As such, Inject() and Extract() implementations that
+    /// call the TextMapWriter and TextMapReader interfaces must agree on a
+    /// prefix or other convention to distinguish their own key:value pairs.
+    ///
+    /// The "foreach" callback pattern reduces unnecessary copying in some cases
+    /// and also allows implementations to hold locks while the map is read.
+    fn foreach_key(&self, f: F) -> Result<()>;
+}
+
+/// TextMapWriter is the Inject() carrier for the TextMap builtin format. With
+/// it, the caller can encode a SpanContext for propagation as entries in a map
+/// of unicode strings.
+///
+/// See the HTTPHeaders examples.
+pub(crate) trait TextMapWriter {
+    /// Set a key:value pair to the carrier. Multiple calls to Set() for the
+    /// same key leads to undefined behavior.
+    ///
+    /// NOTE: The backing store for the TextMapWriter may contain data unrelated
+    /// to SpanContext. As such, Inject() and Extract() implementations that
+    /// call the TextMapWriter and TextMapReader interfaces must agree on a
+    /// prefix or other convention to distinguish their own key:value pairs.
+    fn set(&mut self, key: &str, value: &str) -> Result<()>;
+}
+
+/// HTTPHeadersReader is the Extract() carrier for the HttpHeaders builtin
+/// format. With it, the caller can decode a SpanContext from entries in HTTP
+/// request headers.
+pub(crate) trait HTTPHeadersReader<F>: TextMapReader<F>
+where
+    F: Fn(&str, &str) -> Result<()>,
+{
+}
+
+/// HTTPHeadersWriter is the Inject() carrier for the TextMap builtin format.
+/// With it, the caller can encode a SpanContext for propagation as entries in
+/// http request headers
+pub(crate) trait HTTPHeadersWriter: TextMapWriter {}
+
+/// CustomCarrierReader is the Extract() carrier for a custom format. With it,
+/// the caller can decode a SpanContext from entries in a custom protocol.
+pub(crate) trait CustomCarrierReader<F>
+where
+    F: Fn(&str, &str) -> bool,
+{
+    /// Extract is expected to specialize on the tracer implementation so as to
+    /// most efficiently decode its context.
+    fn extract(&self, tracer: &Tracer) -> Result<Box<dyn SpanContext<F>>>;
+}
+
+/// CustomCarrierWriter is the Inject() carrier for a custom format.  With it,
+/// the caller can encode a SpanContext for propagation as entries in a custom
+/// protocol.
+pub(crate) trait CustomCarrierWriter<F>
+where
+    F: Fn(&str, &str) -> bool,
+{
+    /// Inject is expected to specialize on the tracer implementation so as to most
+    /// efficiently encode its context.
+    fn inject(tracer: &Tracer, sc: &dyn SpanContext<F>) -> Result<()>;
+}
